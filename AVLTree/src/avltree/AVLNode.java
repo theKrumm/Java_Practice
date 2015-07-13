3e34999588fd6d9550041b51caa387a0a7854805
theKrumm <tbraden@uw.edu>
2015-07-13 07:53:25 -0700

    AVL Tree class

---
+++ AVLTree/src/avltree/AVLNode.java
@@ -0,0 +1,18 @@
+package avltree;
+
+public class AVLNode {
+	 public AVLNode left;
+	 public AVLNode right;
+	 public AVLNode parent;
+	 public int key;
+	 public int balance;
+
+	 public AVLNode(int k) {
+	  left = right = parent = null;
+	  balance = 0;
+	  key = k;
+	 }
+	 public String toString() {
+	  return "" + key;
+	 }
+}
